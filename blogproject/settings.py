cfab7d04b6fe6755420459a000a750fe6bd27471
YK <bignfine@naver.com>
2019-07-31 16:04:12 +0900

    ca

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,3 +134,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
